03a178aa6fed13c4141ceeb6a6a0d65a3f9e0ffc
TimpleDhivya <dhivyadhans@gmail.com>
2022-02-04 23:21:54 +0530

    first git

---
+++ src/org/gittest/Emp.java
@@ -0,0 +1,17 @@
+package org.gittest;
+
+public class Emp {
+	private void empName() {
+		System.out.println("dhivya");
+
+	}
+	private void empId() {
+		System.out.println("1234");
+	}
+	public static void main(String[] args) {
+		Emp e = new Emp();
+		e.empId();
+		e.empName();
+	}
+
+}
